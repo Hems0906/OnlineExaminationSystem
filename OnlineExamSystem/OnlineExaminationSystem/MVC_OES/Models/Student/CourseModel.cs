26c7fd472464a979c20ef7960c1fbf8f24dd1ea4
Yogesh Velu <yogeshv@infinite.com>
2025-09-09 10:06:18 +0530

    Mail checking

--- OnlineExamSystem/OnlineExaminationSystem/MVC_OES/Models/Student/CourseModel.cs
+++ OnlineExamSystem/OnlineExaminationSystem/MVC_OES/Models/Student/CourseModel.cs
@@ -8,13 +8,6 @@ namespace MVC_OES.Models.Student
 {
     public class CourseModel
     {
-        //public int course_id { get; set; }
-        //public string course_name { get; set; }
-        //public int next_level { get; set; }
-        //public List<LevelViewModel> levels { get; set; }
-        //public string Status { get; set; } 
-        //public bool CanStartExam { get; set; }
-
         public int course_id { get; set; }
         public string course_name { get; set; }
         public int next_level { get; set; }
